9499f093e04d970977eb8a7cfe105d56cf5887b6
sharon <sharonasprilla1724@gmail.com>
2026-03-09 20:49:59 -0500

    Agrego entidad Usuario y repositorio JPA

---
+++ src/main/java/com/proyecto/knowly_back/KnowlyBackApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.knowly_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowlyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowlyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/knowly_back/KnowlyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.knowly_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowlyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
