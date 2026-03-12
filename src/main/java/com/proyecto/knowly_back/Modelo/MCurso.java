f29beda7f2c2ab36f3032d886063c2a29cd24784
sharon <sharonasprilla1724@gmail.com>
2026-03-11 23:15:43 -0500

    clases

--- src/main/java/com/proyecto/knowly_back/Modelo/MCurso.java
+++ src/main/java/com/proyecto/knowly_back/Modelo/MCurso.java
@@ -1,4 +1,71 @@
 package com.proyecto.knowly_back.Modelo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "curso")
 public class MCurso {
+
+    @Id
+    @Column(name = "idecurso",length = 15,nullable = false)
+    String ideCurso;
+
+    @Column (length = 80,nullable = false)
+    String nombreCurso;
+
+    @Column (length = 250,nullable = false)
+    String descripcion;
+
+    @Column (nullable = false)
+    Integer precio;
+
+    //relaciones
+
+    //constructores
+
+
+    public MCurso(String ideCurso, String nombreCurso, String descripcion, Integer precio) {
+        this.ideCurso = ideCurso;
+        this.nombreCurso = nombreCurso;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public MCurso() {
+    }
+
+    public String getIdeCurso() {
+        return ideCurso;
+    }
+
+    public void setIdeCurso(String ideCurso) {
+        this.ideCurso = ideCurso;
+    }
+
+    public String getNombreCurso() {
+        return nombreCurso;
+    }
+
+    public void setNombreCurso(String nombreCurso) {
+        this.nombreCurso = nombreCurso;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
 }
--- src/main/java/com/proyecto/knowly_back/Modelo/MUsuario.java
+++ src/main/java/com/proyecto/knowly_back/Modelo/MUsuario.java
@@ -1,4 +1,96 @@
 package com.proyecto.knowly_back.Modelo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuario")
 public class MUsuario {
+    @Id
+    @Column(name="idusuario",length = 90,nullable=false)
+    String ideUsuario;
+
+    @Column (length = 90,nullable = false)
+    String nombre;
+
+    @Column(length = 90,nullable = false)
+    String apellido;
+
+    @Column(length = 50,nullable = false)
+    String documento;
+
+    @Column(length = 100,nullable = false)
+    String correo;
+
+    @Column(length = 8,nullable = false)
+    String contrasenia;
+
+    //relaciones
+
+    // constructor
+
+    public MUsuario(String ideUsuario, String nombre, String apellido, String documento, String correo, String contrasenia) {
+        this.ideUsuario = ideUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    public MUsuario() {
+    }
+
+    //encapsulamiento
+
+
+    public String getIdeUsuario() {
+        return ideUsuario;
+    }
+
+    public void setIdeUsuario(String ideUsuario) {
+        this.ideUsuario = ideUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
 }
